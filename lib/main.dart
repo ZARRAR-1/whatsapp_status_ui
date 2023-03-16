382f401e846109993cfd2c5e342f30933ddf0f87
BilluMeow <syed.abeer.hussain.23@gmail.com>
2023-03-16 18:23:00 +0500

    statusCreator() to create list tile of different status

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_interpolation_to_compose_strings
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -37,32 +39,63 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget bodyCreator() {
-    return Column(
+    return ListView(
       children: [
         secondAppBar(),
-        ListTile(
-          leading: CircleAvatar(
-            backgroundColor: Colors.transparent,
-            // ignore: prefer_const_literals_to_create_immutables
-            child: Stack(children: [
-              Image.asset("assets/flutter.jpg"),
-              const Align(
-                alignment: Alignment.bottomRight,
-                child: CircleAvatar(
-                  radius: 9,
-                  backgroundColor: Colors.white,
-                  child: Icon(
-                    Icons.add,
-                    size: 17,
-                  ),
-                ),
-              ),
-            ]),
-          ),
-          title: const Text("MY STATUS"),
-          subtitle: const Text("Tap to add status update"),
-        ),
+        myStatusCreator(),
+        textContainerWithOpacity("Recent Updates"),
+        statusCreator("Maarij", "2 Minutes ago", "1.jpg"),
+        statusCreator("Anish", "5 Minutes ago", "2.jpg"),
+        statusCreator("Mohtashim", "Today, 5:00 AM", "3.jpg"),
+        textContainerWithOpacity("Viewed Updates"),
+        statusCreator("Wasif", "Today, 5:00 PM", "4.jpg"),
+        statusCreator("Abeer", "Today, 5:55 AM", "5.jpg"),
+        statusCreator("Taha", "Yesterday, 7:23 PM", "6.jpg"),
+        statusCreator("Baqar", "Yesterday, 5:00 PM", "7.jpg"),
       ],
+    );
+  }
+
+  Container textContainerWithOpacity(var text) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 30,
+      color: const Color(0x00000000).withOpacity(0.2),
+      child:
+          Align(alignment: Alignment.centerLeft, child: Text("     " + text)),
+    );
+  }
+
+  ListTile myStatusCreator() {
+    return const ListTile(
+      leading: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        backgroundImage: AssetImage("assets/flutter.jpg"),
+        child: Align(
+          alignment: Alignment.bottomRight,
+          child: CircleAvatar(
+            radius: 9,
+            backgroundColor: Colors.white,
+            child: Icon(
+              Icons.add,
+              size: 17,
+            ),
+          ),
+        ),
+      ),
+      title: Text("MY STATUS"),
+      subtitle: Text("Tap to add status update"),
+    );
+  }
+
+  ListTile statusCreator(var name, var time, var imagename) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        backgroundImage: AssetImage("assets/" + imagename),
+      ),
+      title: Text(name),
+      subtitle: Text(time),
     );
   }
 
